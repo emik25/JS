a8e46d8797005623a1fdec445986933e68d8bbe3
Emil <115448156+emik25@users.noreply.github.com>
2022-10-26 10:41:42 +0500

    Add files via upload

---
+++ scr/script.js
@@ -0,0 +1,20 @@
+// 1
+let int = prompt('any numbers')
+
+if(int %2===0){
+    console.log('true');
+}else{
+    console.log('false');
+}
+
+
+// 2
+let name = prompt('What is your name?')
+
+let age = prompt('How old are u?')
+
+if(name === 'A' && age >=18){
+    console.log('Welcome');
+} else{
+    console.log('Go home');
+}
